9d619e9424a2a91db9ca56a74f2944f1f0b476e0
Singh_Shailesh <shaileshsingh12396@gmail.com>
2024-02-05 17:43:39 +0530

    2

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,13 +1,33 @@
 import logo from './logo.svg';
+import React, {useEffect,useRef} from 'react';
 import './App.css';
 import { FaBeer } from "react-icons/fa";
+import lottie from 'lottie-web';
+import Lottie from 'lottie-react';
+import animationData from './Resources/check.json'
+
 function App() {
+  // const container=useRef(null);
+  // useEffect(()=>{
+  //   lottie.loadAnimation({
+  //     container:container.current,
+  //     renderer:'svg',
+  //     loop:true,
+  //     autoplay:true,
+  //     animationData:requestAnimationFrame('./check.json')
+  //   })
+  // },[])
   return (
     <div className="App">
       <h3>
       Lets go for a <FaBeer />?
+      
     </h3>
-    </div>
+    
+    <div>
+        <Lottie height={40} animationData={animationData}/>
+      </div>
+      </div>
   );
 }
 
